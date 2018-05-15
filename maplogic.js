514e51fe000c1875b6b63849d7740b9784cd7c40
Unknown <1540621110@qq.com>
2018-05-15 17:39:33 +0100

    ff

--- maplogic.js
+++ maplogic.js
@@ -50,7 +50,7 @@ function initialise() {
 				var thisIcon = icon5;
 			}
 		var markerb = L.marker([myData[item].latitude,myData[item].longitude],{icon: thisIcon, title:"The id of the earthquake is " + myData[item].id}).addTo(myMapB);
-		markerb.bindPopup("The more information of the earthquake：<br> Date: "+ myData[item].date + "<br> Time: " + myData[item].time +"<br> Magnitude:" +
+			markerb.bindPopup("The more information of the earthquake：<br> Date: "+ myData[item].date + "<br> Time: " + myData[item].time +"<br> Magnitude:" +
 			myData[item].magnitude +  "<br> Influence distance: " + myData[item].horizontal_distance + "km");
 		var markea = L.marker([myData[item].latitude,myData[item].longitude]).addTo(myMapA);
 		var circle = L.circle([myData[item].latitude,myData[item].longitude],{radius:myData[item].horizontal_distance * 1000}).addTo(myMapB);
@@ -88,60 +88,4 @@ function initialise() {
 	myMapA.on('click',onClickA);
 	myMapB.on('click',onClickB);
 	myMapA.on('dragend',onDragA);
-
-	require.config({
-            paths: {
-                echarts: 'http://echarts.baidu.com/build/dist'
-            }
-        });
-
-         require(
-            [
-                'echarts',
-                'echarts/chart/pie'
-            ],
-            function (ec) {
-                var myChart = ec.init(document.getElementById('chartarea'));
-                tooltip: {
-                        show: true
-                    },
-	            legend: {
-				        orient: 'vertical',
-				        x: 'left',
-				        data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-				    },
-				series: [
-			        {
-			            name:'访问来源',
-			            type:'pie',
-			            radius: ['50%', '70%'],
-			            avoidLabelOverlap: false,
-			            label: {
-			                normal: {
-			                    show: false,
-			                    position: 'center'
-			                },
-			                emphasis: {
-			                    show: true,
-			                    textStyle: {
-			                        fontSize: '30',
-			                        fontWeight: 'bold'
-			                    }
-			                }
-			            },
-			            labelLine: {
-			                normal: {
-			                    show: false
-			                }
-			            },
-			            data:[
-			                {value:335, name:'直接访问'},
-			                {value:310, name:'邮件营销'},
-			                {value:234, name:'联盟广告'},
-			                {value:135, name:'视频广告'},
-			                {value:1548, name:'搜索引擎'}
-			            ]
-			        }
-			    ]
-            }
 }
