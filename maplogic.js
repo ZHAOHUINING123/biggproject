20d3d0fdfc615c643e50883179cb4667d343129f
Unknown <1540621110@qq.com>
2018-05-15 16:14:50 +0100

    gf

--- maplogic.js
+++ maplogic.js
@@ -89,4 +89,59 @@ function initialise() {
 	myMapB.on('click',onClickB);
 	myMapA.on('dragend',onDragA);
 
+	require.config({
+            paths: {
+                echarts: 'http://echarts.baidu.com/build/dist'
+            }
+        });
+
+         require(
+            [
+                'echarts',
+                'echarts/chart/pie'
+            ],
+            function (ec) {
+                var myChart = ec.init(document.getElementById('chartarea'));
+                tooltip: {
+                        show: true
+                    },
+	            legend: {
+				        orient: 'vertical',
+				        x: 'left',
+				        data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+				    },
+				series: [
+			        {
+			            name:'访问来源',
+			            type:'pie',
+			            radius: ['50%', '70%'],
+			            avoidLabelOverlap: false,
+			            label: {
+			                normal: {
+			                    show: false,
+			                    position: 'center'
+			                },
+			                emphasis: {
+			                    show: true,
+			                    textStyle: {
+			                        fontSize: '30',
+			                        fontWeight: 'bold'
+			                    }
+			                }
+			            },
+			            labelLine: {
+			                normal: {
+			                    show: false
+			                }
+			            },
+			            data:[
+			                {value:335, name:'直接访问'},
+			                {value:310, name:'邮件营销'},
+			                {value:234, name:'联盟广告'},
+			                {value:135, name:'视频广告'},
+			                {value:1548, name:'搜索引擎'}
+			            ]
+			        }
+			    ]
+            }
 }
